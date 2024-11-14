b81ee430399dc66635106c849a3447b57603b90d
PGI-Uni <pgarciaibanez6@gmail.com>
2024-11-14 14:14:55 +0100

    Retoques
    Añadido a la primera parte 3 detalles:
    -Archivos a ignorar de la parte 1
    -El Escuchador (vacio de momento)
    -Modificaciones al Main para el acceso a los datos y la llamada al arbol (he omitido la version anterior para su revision)

---
+++ parte1.2/gramatica/Escuchador.java
@@ -0,0 +1,7 @@
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+public class Escuchador extends MapaParserBaseListener{
+    
+}
